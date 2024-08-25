60d4b414db67856b750fb678a14cb464adea93d3
Jessica-Ruiz <ruizrinconjessicajineth@gmail.com>
2024-08-24 23:23:15 -0500

    Se agregaron los diferentes tipod de variables

---
+++ tipo_variable.py
@@ -0,0 +1,11 @@
+#Tipos de variables
+cadena="github"
+print (type(cadena))
+numero=10
+print (type(numero))
+decimal=11.190
+print (type(decimal))
+bool=True
+print (type(bool))
+letra="a"
+print (type(letra))
