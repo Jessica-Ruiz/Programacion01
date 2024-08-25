1672a63f6b2ca23eefadb24d1cfeecc230038530
Jessica-Ruiz <ruizrinconjessicajineth@gmail.com>
2024-08-24 23:33:53 -0500

    Guardar todo lo cambios

---
+++ captura_informacion.py
@@ -0,0 +1,6 @@
+#Capturar Informacion
+nombre=print("Escriba su nombre completo: ")
+cedula=int(print("Digitalize su numero de cedula: "))
+estatura=float(print("Digite su estatura: "))
+estado=bool(print("Digite su estado: "))
+print(f"Su nombre es {nombre},su cedula es{cedula},su estatura es{estatura} y su estado es {estado}")
